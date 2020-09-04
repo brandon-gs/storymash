50a2fab0696123f5a90e25a4a7179a2a8da4dc15
Brandon Garcia <brandongs180@outlook.com>
2020-09-03 20:29:05 -0500

    Add copyright component

---
+++ frontend/components/Copyright/index.tsx
@@ -0,0 +1,15 @@
+import { Typography } from "@material-ui/core"
+import { Link } from ".."
+
+export default function Copyright(): JSX.Element {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://brandongs.herokuapp.com/">
+        BrandonGS
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  )
+}
