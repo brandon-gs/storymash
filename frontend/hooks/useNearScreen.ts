2ef34b6691b664dd71a035cbabd9d9a65a6d53de
Brandon García <cosmoscrew1@gmail.com>
2020-10-26 15:16:03 -0600

    Add useNearScreen hook

---
+++ frontend/hooks/useNearScreen.ts
@@ -0,0 +1,41 @@
+// Hooks
+import { useEffect, useRef, useState } from "react"
+
+type Props = {
+  distance?: number
+  once?: boolean
+}
+
+export default function useNearScreen({ distance = 100, once = true }: Props) {
+  const [isNearScreen, setIsNearScreen] = useState(false)
+  const fromRef = useRef<Element>(null)
+
+  useEffect(() => {
+    let observer: IntersectionObserver
+    const onChange: IntersectionObserverCallback = (entries: IntersectionObserverEntry[]) => {
+      const el = entries[0]
+      if (el.isIntersecting) {
+        setIsNearScreen(true)
+        once && observer.disconnect()
+      } else {
+        !once && setIsNearScreen(false)
+      }
+    }
+
+    Promise.resolve(
+      typeof IntersectionObserver !== "undefined"
+        ? IntersectionObserver
+        : import("intersection-observer")
+    ).then(() => {
+      observer = new IntersectionObserver(onChange, {
+        rootMargin: `${distance}px`,
+      })
+      if (fromRef.current) {
+        observer.observe(fromRef.current)
+      }
+    })
+    return () => observer && observer.disconnect()
+  })
+
+  return { isNearScreen, fromRef }
+}
