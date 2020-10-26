cd98d805910bf5bcf97e15563aef8707ffce3a60
Brandon García <cosmoscrew1@gmail.com>
2020-10-26 15:09:30 -0600

    Add Spinner component

---
+++ frontend/components/Common/Spinner/styles.ts
@@ -0,0 +1,11 @@
+import { makeStyles } from "@material-ui/core/styles"
+
+export default makeStyles(theme => ({
+  spinnerContainer: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: theme.spacing(5, "auto"),
+  },
+}))
